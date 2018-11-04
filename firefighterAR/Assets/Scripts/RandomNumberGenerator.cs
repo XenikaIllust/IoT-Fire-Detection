7eecca41cb5b0624cbe029cc7a9de2f1b904a310
Nhan Tran <nhantrantnt@gmail.com>
2018-11-04 09:04:30 -0800

    added app for epson AR glasses

---
+++ firefighterAR/Assets/Scripts/RandomNumberGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class RandomNumberGenerator : MonoBehaviour {
+	public Text temperatures;
+	private float nextActionTime = 0.0f;
+	public float period = 0.3f;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextActionTime) {
+			nextActionTime += period;
+			int randomNumber = Random.Range (200, 205);
+			Debug.Log (randomNumber);
+			temperatures.text = "Floor 1 temperature: " + randomNumber.ToString ();
+		}
+	}
+}
---
+++ firefighterAR/Assets/Scripts/textDelayPrint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class textDelayPrint : MonoBehaviour {
+	public Text warning;
+	private float nextActionTime = 0.0f;
+	public float period = 0.3f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextActionTime) {
+			nextActionTime += period;
+			int randomNumber = Random.Range (1,3);
+			warning.text = "Fire and CO detected in <First> floor. Potential "+ randomNumber + " victim(s).";
+		}
+	}
+}
